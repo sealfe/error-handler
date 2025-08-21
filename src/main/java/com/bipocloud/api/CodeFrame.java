af6764e1a413632075e03c78768174baeed2e83a
wangchuan <15935144396@163.com>
2025-08-21 00:27:43 -0700

    Add code record generation with git context

---
+++ src/main/java/com/bipocloud/api/CodeFrame.java
@@ -0,0 +1,33 @@
+package com.bipocloud.api;
+
+import java.util.List;
+
+public class CodeFrame {
+    private final String file;
+    private final int line;
+    private final String author;
+    private final List<String> code;
+
+    public CodeFrame(String file, int line, String author, List<String> code) {
+        this.file = file;
+        this.line = line;
+        this.author = author;
+        this.code = code;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public List<String> getCode() {
+        return code;
+    }
+}
---
+++ src/main/java/com/bipocloud/api/CodeRecord.java
@@ -0,0 +1,27 @@
+package com.bipocloud.api;
+
+import java.util.List;
+
+public class CodeRecord {
+    private final String type;
+    private final String code;
+    private final List<CodeFrame> stack;
+
+    public CodeRecord(String type, String code, List<CodeFrame> stack) {
+        this.type = type;
+        this.code = code;
+        this.stack = stack;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<CodeFrame> getStack() {
+        return stack;
+    }
+}
